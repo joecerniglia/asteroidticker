fecb43ccf1fa6de7a70c768ff88b9c084e1508d8
joecerniglia <97735240+joecerniglia@users.noreply.github.com>
2022-11-20 15:39:21 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -191,7 +191,8 @@ def reportout(pagenum):
 
     return render_template('form2.html', report=PageResult(report, int(pagenum.replace('.0','')), pn),
     calday=calday,complete_date=complete_date,LD=LD,
-    daysago=daysago,count=count,lastpage=lastpage)
+    daysago=daysago,count=count,lastpage=int(lastpage.replace('.0',''))
+
 
 
 if __name__ == '__main__':
