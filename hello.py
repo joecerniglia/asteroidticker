f2d0535145a0fa30ed8dbe653830264e50628f44
joecerniglia <97735240+joecerniglia@users.noreply.github.com>
2022-11-22 16:32:29 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -32,9 +32,9 @@ bootstrap = Bootstrap(app)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#app.config['SQLALCHEMY_DATABASE_URI'] =\
+    #'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
