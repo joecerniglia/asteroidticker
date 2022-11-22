147a38211e9627ecf6ca0c0484b6c1a9c6db582b
joecerniglia <97735240+joecerniglia@users.noreply.github.com>
2022-11-22 16:35:00 -0500

    Delete import os from flask_qa import create_app  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL).replace('postgres:/', 'postgresql: directory

--- import os from flask_qa import create_app  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL).replace('postgres:/', 'postgresql:/setting.py
+++
@@ -1,7 +0,0 @@
-import os
-from flask_qa import create_app
-
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL).replace('postgres://', 'postgresql://')
-#SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-SECRET_KEY = os.environ.get('SECRET_KEY')
-SQLALCHEMY_TRACK_MODIFICATIONS = False
