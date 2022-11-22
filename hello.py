7ee88cc04792e5aab748ccaff1f9f87a97b5adf6
joecerniglia <97735240+joecerniglia@users.noreply.github.com>
2022-11-22 08:47:02 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -206,7 +206,7 @@ class PageResult:
 @app.route('/reportout/<pagenum>', methods=['GET'])
 def reportout(pagenum):
 
-    time.sleep(6+count*.5)
+    #time.sleep(6+count*.5)
 
     return render_template('form2.html', report=PageResult(report, int(pagenum), pn),
     calday=calday,complete_date=complete_date,LD=LD,
